101ac972a66525d84e1106a956d2f0ec1beb27ae
Daniel2231 <daniel2231.dev@gmail.com>
2022-09-20 16:47:27 +0900

    first init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'arrow-body-style': ['error', 'always']
+  }
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResponsiveAppBar from '../components/common/Appbar';
+
+const Home = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <h1>Home</h1>
+      <h3>Test</h3>
+    </>
+  );
+};
+
+export default Home;
