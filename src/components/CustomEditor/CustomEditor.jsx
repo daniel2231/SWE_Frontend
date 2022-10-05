85659ce09ad7c03385a6dcd23184c2edd038e80b
2sseul2 <2sseul2yoo@gmail.com>
2022-10-05 14:19:22 +0900

    Add button's action in editor block except button using terminal

--- src/components/CustomEditor/CustomEditor.jsx
+++ src/components/CustomEditor/CustomEditor.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 
 import Editor from '@monaco-editor/react';
 import styled from '@emotion/styled';
 import Box from '@mui/material/Box';
-
 import { Button, Typography } from '@mui/material';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import FolderSharpIcon from '@mui/icons-material/FolderSharp';
@@ -11,40 +10,157 @@ import RestartAltSharpIcon from '@mui/icons-material/RestartAltSharp';
 import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 
+const skeletonCode = 'function solution(n){\n  const test=0;\n}';
+const saveLabelNumber = [1, 2, 3];
+
 const CustomEditor = () => {
+  const [currentLabel, setCurrentLabel] = React.useState(1);
+  const handleCurrentLabel = (number) => {
+    setCurrentLabel(number);
+  };
+
+  const [editorContent, setEditorContent] = React.useState({
+    1: '',
+    2: '',
+    3: ''
+  });
+  const [saveContent, setSaveContent] = React.useState({
+    1: '',
+    2: '',
+    3: ''
+  });
+
+  // 에디터 내용이 바뀔때마다 editorContent를 세팅
+  const handleEditorChange = (value) => {
+    setEditorContent((prev) => ({
+      ...prev,
+      [currentLabel]: value
+    }));
+  };
+
+  // 저장 버튼을 누르면 해당하는 라벨의 saveContent에 editorContent를 세팅
+  const handleSaveButton = () => {
+    setSaveContent((prev) => ({
+      ...prev,
+      [currentLabel]: editorContent[currentLabel]
+    }));
+
+    alert('저장 완료');
+  };
+
+  const DisplayFileContent = (file) => {
+    const fr = new FileReader();
+    fr.readAsText(file, 'UTF-8');
+    fr.onload = () => {
+      setSaveContent((prev) => ({
+        ...prev,
+        [currentLabel]: fr.result
+      }));
+      setEditorContent((prev) => ({
+        ...prev,
+        [currentLabel]: fr.result
+      }));
+    };
+  };
+
+  const handleFolderOpenButton = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'text/plain, .js';
+    input.click();
+    input.onchange = (event) => {
+      DisplayFileContent(event.target.files[0]);
+    };
+  };
+
+  // reset을 누르면 초기화 코드로 다시 저장됨.
+  const handleResetButton = () => {
+    setSaveContent((prev) => ({
+      ...prev,
+      [currentLabel]: skeletonCode
+    }));
+  };
+
+  // 현재 작성 중인 코드를 복사함(저장안해도)
+  const handleCopyButton = () => {
+    window.navigator.clipboard.writeText(editorContent[currentLabel]).then(() => {
+      alert('복사 완료');
+    });
+  };
+
+  // 현재 작성 중인 코드를 다운함(저장안해도)
+  const handleDownloadButton = () => {
+    // Blob 생성자: 파일로 만들고자 하는것, 그것의 타입
+    const blob = new Blob([editorContent[currentLabel]], { type: 'text/plain' });
+
+    // Blob 객체를 나타내는 다운로드 url 생성
+    const url = window.URL.createObjectURL(blob);
+
+    // 다운로드 기능만 수행하는 a 태그 생성
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `file${currentLabel}.js`;
+    a.click();
+
+    // 태그 및 url 삭제: 메모리 누수 방지
+    a.remove();
+    window.URL.revokeObjectURL(url);
+  };
+
+  const handleRunButton = () => {};
+  const handleGradeButton = () => {};
+  const handleSubmitButton = () => {};
+
   return (
     <>
       <Container sx={{ borderBottom: '.1px solid black' }}>
         <Typography sx={{ fontSize: '20px', fontWeight: 'bold' }}>코드 입력</Typography>
         <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
-          <Label>1</Label>
-          <Label>2</Label>
-          <Label>3</Label>
-          <SaveOutlinedIcon sx={{ cursor: 'pointer', marginLeft: '1rem' }} />
+          {saveLabelNumber.map((item) => (
+            <Label
+              onClick={() => {
+                handleCurrentLabel(item);
+              }}
+              sx={{
+                borderRadius: currentLabel === item ? '1rem 0 0 0' : ''
+              }}
+              key={item}
+            >
+              {item}
+            </Label>
+          ))}
+
+          <SaveOutlinedIcon
+            sx={{ cursor: 'pointer', marginLeft: '1rem' }}
+            onClick={handleSaveButton}
+          />
         </Box>
       </Container>
 
       <Editor
         defaultLanguage="javascript"
-        defaultValue="// 코드를 입력하세요."
+        value={saveContent[currentLabel] || skeletonCode}
+        onChange={handleEditorChange}
         style={{ height: '70vh' }}
       />
 
       <Container sx={{ borderTop: '.1px solid black' }}>
         <Box sx={{ display: 'flex', gap: 2 }}>
-          <FolderSharpIcon style={{ cursor: 'pointer' }} />
-          <RestartAltSharpIcon style={{ cursor: 'pointer' }} />
-          <ContentCopyOutlinedIcon style={{ cursor: 'pointer' }} />
-          <FileDownloadOutlinedIcon style={{ cursor: 'pointer' }} />
+          <FolderSharpIcon style={{ cursor: 'pointer' }} onClick={handleFolderOpenButton} />
+          <RestartAltSharpIcon style={{ cursor: 'pointer' }} onClick={handleResetButton} />
+          <ContentCopyOutlinedIcon style={{ cursor: 'pointer' }} onClick={handleCopyButton} />
+          <FileDownloadOutlinedIcon style={{ cursor: 'pointer' }} onClick={handleDownloadButton} />
         </Box>
 
-        <Box sx={{ display: 'flex', gap: 5 }}>
-          <Box sx={{ display: 'flex', gap: 2 }}>
-            <ActionButton>실행</ActionButton>
-            <ActionButton>채점</ActionButton>
+        <Box sx={{ display: 'flex', gap: 3 }}>
+          <Box sx={{ display: 'flex', gap: 1 }}>
+            <ActionButton onClick={handleRunButton}>실행</ActionButton>
+            <ActionButton onClick={handleGradeButton}>채점</ActionButton>
           </Box>
 
-          <ActionButton style={{ backgroundColor: '#409BFF' }}>제출</ActionButton>
+          <ActionButton style={{ backgroundColor: '#409BFF' }} onClick={handleSubmitButton}>
+            제출
+          </ActionButton>
         </Box>
       </Container>
     </>
@@ -69,7 +185,6 @@ const Label = styled(Box)`
   cursor: pointer;
   &:hover {
     background-color: #8faccd;
-    // 활성화중인 색 : 183C65
   }
 `;
 
