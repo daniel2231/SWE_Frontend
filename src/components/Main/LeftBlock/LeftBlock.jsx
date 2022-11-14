3e2fd9796c8100fdfc58f313ffc4d7238c2cf502
2sseul2 <2sseul2yoo@gmail.com>
2022-11-14 15:30:15 +0900

    Update testcase test button, display result, not completed

--- src/components/Main/LeftBlock/LeftBlock.jsx
+++ src/components/Main/LeftBlock/LeftBlock.jsx
@@ -4,7 +4,6 @@ import styled from '@emotion/styled';
 import Title from './Title';
 import TestcaseBlock from './TestcaseBlock';
 import Content from './Content';
-import TestSuccessContext from '../../../context/TestSuccessContext';
 
 const testQuestion =
   '피보나치 수는 0과 1로 시작하며, 다음 피보나치 수는 바로 앞의 두 피보나치 수의 합이 된다. n = 0, 1...에 해당하는 피보나치 수는 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...이다. n번째 피보나치 수를 리턴하시오.';
@@ -21,17 +20,6 @@ const testcase = [
 ];
 
 const LeftBlock = () => {
-  const [yourOutput, setYourOutput] = React.useState('');
-  const [testSuccess, setTestSuccess] = React.useState(false);
-  const value = React.useMemo(
-    () => ({
-      yourOutput,
-      setYourOutput,
-      testSuccess,
-      setTestSuccess
-    }),
-    [yourOutput, setYourOutput, testSuccess, setTestSuccess]
-  );
   return (
     <Container>
       <Title title="문제 & 참조/제약사항" />
@@ -43,9 +31,7 @@ const LeftBlock = () => {
       <Title title="테스트케이스" />
       <TestcaseContainer>
         {testcase.map((item) => (
-          <TestSuccessContext.Provider value={value}>
-            <TestcaseBlock subTitle={item.title} input={item.input} output={item.output} />
-          </TestSuccessContext.Provider>
+          <TestcaseBlock subTitle={item.title} input={item.input} output={item.output} />
         ))}
       </TestcaseContainer>
     </Container>
